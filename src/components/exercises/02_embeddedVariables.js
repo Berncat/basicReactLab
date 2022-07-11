a1f6cd13062f94894eeb0c75e4a78c81424f4bdd
Berncat <b.cattigan1@hotmail.com>
2022-07-11 22:02:11 +0100

    Excerice 2 completed

--- src/components/exercises/02_embeddedVariables.js
+++ src/components/exercises/02_embeddedVariables.js
@@ -1,10 +1,47 @@
 import React from "react";
 import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
-
 const Demo = () => {
+  const modules = [
+    {
+      name: "DevOps",
+      lectures: 2,
+      practicals: 3,
+    },
+    {
+      name: "Enterprise Web Dev",
+      lectures: 3,
+      practicals: 2,
+    },
+  ];
+  const header = "HDip Computer Science - Modules table";
   return (
-      <h1>TODO</h1>
+    <>
+      <h1>{header}</h1>
+      <div>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>No lectures</th>
+              <th>No practicals</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{modules[0].name}</td>
+              <td>{modules[0].lectures}</td>
+              <td>{modules[0].practicals}</td>
+            </tr>
+            <tr>
+              <td>{modules[1].name}</td>
+              <td>{modules[1].lectures}</td>
+              <td>{modules[1].practicals}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
